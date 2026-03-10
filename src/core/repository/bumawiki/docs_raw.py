d59436eeb6cefdfae70138bc512ce3999554a2f2
pdh0128 <pdh0128a@gmail.com>
2026-03-10 16:06:31 +0900

    delete :: 안쓰이는 docs parquet 변환 제거

--- src/core/repository/bumawiki/docs_raw.py
+++ src/core/repository/bumawiki/docs_raw.py
@@ -4,9 +4,5 @@ from abc import ABC, abstractmethod
 class BumaWikiDocsRawRepository(ABC):
 
     @abstractmethod
-    def save(self, ds: str) -> None:
-        raise NotImplementedError
-
-    @abstractmethod
     def exists(self, ds: str, title: str) -> bool:
         raise NotImplementedError
